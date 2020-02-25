7710f8285ead402362848fe68b56d8a6d8561014
healstyle <healstyle@163.com>
2020-02-25 10:03:20 +0800

    用方向建控制Tank的移动和停止

--- src/com/shentu/tank/TankFrame.java
+++ src/com/shentu/tank/TankFrame.java
@@ -14,6 +14,9 @@ public class TankFrame extends Frame {
 	boolean right = false;
 	int x = 200;
 	int y = 200;
+	//刚开始必须要给一个值，如果不给用switch的时候会报空指针
+	Dir dir = Dir.DOWN; 
+	private final int speed = 10;
 	
 	TankFrame () {
 		setSize(800,600);
@@ -33,10 +36,39 @@ public class TankFrame extends Frame {
 	
 	public void paint(Graphics g) {
 		g.fillRect(x, y, 50, 50);
-		y += 20;
+		
+		switch (dir) {
+		case UP:
+			y -= speed;
+			break;
+		case DOWN:
+			y += speed;
+			break;
+		case LEFT:
+			x -= speed;
+			break;
+		case RIGHT:
+			x += speed;
+			break;
+		default:
+			break;
+		}
+		
+		
 	}
 	
+	
 	class MyKeyMonitor extends KeyAdapter {
+		
+		private void setMainTankDir() {
+			if(up) dir = Dir.UP;
+			if(down) dir = Dir.DOWN;
+			if(left) dir = Dir.LEFT;
+			if(right) dir = Dir.RIGHT;
+			if(up == false && down == false && left == false && right == false) {
+				dir = Dir.STOP;
+			}
+		}
 		@Override
 		public void keyReleased(KeyEvent e) {
 			int key = e.getKeyCode();
@@ -53,10 +85,11 @@ public class TankFrame extends Frame {
 			case KeyEvent.VK_RIGHT:
 				right = false;
 				break;
-
 			default:
 				break;
 			}
+			
+			setMainTankDir();
 		}
 
 		@Override
@@ -79,6 +112,8 @@ public class TankFrame extends Frame {
 			default:
 				break;
 			}
+			
+			setMainTankDir();
 		}
 		
 	}
